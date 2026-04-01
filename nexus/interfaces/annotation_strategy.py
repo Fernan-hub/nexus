5b16ef2cefefe4ed4fe7180543d7dd152b6f5641
FernanHub <fangeles@simplai.es>
2026-04-02 00:40:46 +0200

    Clean slate

--- nexus/interfaces/annotation_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any, Sequence
-import neo
-
-class AnnotationStrategy(ABC):
-    @abstractmethod
-    def annotate(self, segment: neo.Segment | Sequence[Any]) -> None:
-        pass
--- nexus/interfaces/processing_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any, Sequence
-
-
-class ProcessingStrategy(ABC):
-	@abstractmethod
-	def apply(self, signals: Sequence[Any]) -> Sequence[Any] | None:
-		pass
