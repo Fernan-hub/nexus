268dc70d7be6613b5bef7b3b4ed20a89cc2f215e
FernanHub <fangeles@simplai.es>
2026-05-12 01:00:31 +0200

    Chore: fixed namings and type hints

--- nexus/core/interfaces.py
+++ nexus/core/interfaces.py
@@ -1,16 +1,15 @@
 import uuid
 
 from abc import ABC, abstractmethod
-from typing import Any
 from neo.core.dataobject import DataObject
+
+from nexus.types import Annotation
 
 
 class SignalProxy(ABC):
-    def __init__(self, annotations: dict[str, Any] | None = None) -> None:
+    def __init__(self, annotations: Annotation | None = None) -> None:
         self._id = uuid.uuid4().hex
-        self._annotations: dict[str, Any] = (
-            annotations if annotations is not None else {}
-        )
+        self._annotations: Annotation = annotations if annotations is not None else {}
         self._cache: DataObject | None = None
 
     @property
@@ -18,7 +17,7 @@ class SignalProxy(ABC):
         return self._id
 
     @property
-    def annotations(self) -> dict[str, Any]:
+    def annotations(self) -> Annotation:
         return self._annotations
 
     @abstractmethod
--- nexus/core/results_exporter.py
+++ nexus/core/results_exporter.py
@@ -4,7 +4,7 @@ from nexus.exportation.interfaces import ExportationStrategy
 
 class ResultsExporter:
     def __init__(self, analysis_results: LazyAnalysisResult):
-        self._lazy_analysisresults = analysis_results
+        self._lazy_analysis_results = analysis_results
 
     def export_result(self, exporter_strategy: ExportationStrategy) -> None:
-        self._lazy_analysisresults.accept(exporter_strategy)
+        self._lazy_analysis_results.accept(exporter_strategy)
