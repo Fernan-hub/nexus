7595e54a6f0b734d1f4e44864b347e9bbd2326e5
FernanHub <fangeles@simplai.es>
2026-05-17 23:12:04 +0200

    Add to_neo_block implementation to NeuroData

--- nexus/core/neuro_data.py
+++ nexus/core/neuro_data.py
@@ -1,6 +1,8 @@
-from neo.core import Block
+from neo.core import Block, Segment
+from neo.core.analogsignal import AnalogSignal
 from neo.core.container import filterdata
 from neo.core.dataobject import DataObject
+from neo.core.spiketrain import SpikeTrain
 from neo.io.proxyobjects import BaseProxy
 
 from nexus.annotation.interfaces import AnnotationStrategy
@@ -48,6 +50,33 @@ class NeuroData:
             signal_proxies.append(signal_proxy)
         annotation_strategy.annotate(signal_proxies)
 
-    def to_neo_block(self) -> Block:
-        # TODO: Implement conversion to Neo block
-        return Block()
+    def to_neo_block(self, criteria_dict: dict[str, Criteria] | None = None) -> Block:
+        block = Block()
+        if criteria_dict is None:
+            proxies = list(self._proxy_registry.values())
+            self._load_proxies_into_new_segment(block, proxies)
+        else:
+            proxies_by_criteria = self.get_proxies_by_criteria_dict(criteria_dict)
+            for name, proxies in proxies_by_criteria.items():
+                self._load_proxies_into_new_segment(block, proxies, seg_name=name)
+        return block
+
+    def _load_proxies_into_new_segment(
+        self, block: Block, proxies: list[SignalProxy], seg_name: str | None = None
+    ) -> None:
+        seg = Segment(name=seg_name)
+        block.segments.append(seg)
+        seg.block = block
+        for proxy in proxies:
+            data = proxy.load()
+            self._load_into_segment(data, seg)
+
+    @staticmethod
+    def _load_into_segment(data: DataObject, seg: Segment) -> None:
+        if isinstance(data, AnalogSignal):
+            seg.analogsignals.append(data)
+        elif isinstance(data, SpikeTrain):
+            seg.spiketrains.append(data)
+        else:
+            return
+        data.segment = seg
